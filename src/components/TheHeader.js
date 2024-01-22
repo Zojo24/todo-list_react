c7884f40efd247882dc49b5aa1a79a4474c991fa
Zojo24 <hchoi2894@gmail.com>
2024-01-22 21:57:41 +0900

    Header 컴포넌트 구성

--- src/components/TheHeader.js
+++ src/components/TheHeader.js
@@ -1,2 +1,36 @@
+import React, { useState } from 'react'
 
-  
+const TheHeader = () => {
+	const [menus] = useState([
+		{
+			name: 'Home',
+			href: '#/'
+		},
+		{
+			name: 'About',
+			href: '#/about'
+		}
+	])
+	return (
+		<header className="header">
+			<div className="header-wrapper">
+				<a href="#/">
+					<img
+						className="bank-logo"
+						src="/TossBank_Logo_Primary_Reverse.png"
+						alt="Toss Bank logo"
+					/>
+				</a>
+				<div className="menu">
+					{menus.map((menu, index) => (
+						<a
+							key={index}
+							href={menu.href}>
+							{menu.name}
+						</a>
+					))}
+				</div>
+			</div>
+		</header>
+	)
+}
